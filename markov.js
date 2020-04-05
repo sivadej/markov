2a40872b92e0678b228ebd469fc3b5d43656e936
Sivadej K <bombyk@gmail.com>
2020-04-04 23:17:28 -0800

    function MarkovMachine.makeText()

--- markov.js
+++ markov.js
@@ -1,27 +1,17 @@
 /** Textual markov chain generator */
-
-
 class MarkovMachine {
-
-	/** build markov machine; read in text.*/
-  
-	// instantiate array of individual words from string arg
+	// make array of individual words from text
 	constructor(text) {
 	  let words = text.split(/[ \r\n]+/);
-	  this.words = words.filter(c => c !== "");
-	  this.makeChains();
+    this.words = words.filter(c => c !== "");
+    this.chains = this.makeChains(this.words);
 	}
-  
-  
-	/** set markov chains:
-	 *
-	 *  for text of "the cat in the hat", chains will be
-	 *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
-  
+
+	// set markov chains:
 	// loop through array. for each word as key, set add next word to value array 
-	makeChains() {
+  makeChains(wordsArr) {
 	  const chains = {};
-	  this.words.forEach((word,idx,arr)=>{
+	  wordsArr.forEach((word,idx,arr)=>{
       //console.log(`${idx}: ${word} ${arr[idx+1]}`);
       //if key exists, add word to array. 
       //else assign a new word array to key
@@ -31,17 +21,40 @@ class MarkovMachine {
       else {
         chains[word] = [arr[idx+1]];
       }
-      
     })
-    console.log(chains);
     return chains;
 	}
   
-  
-	/** return random text from chains */
+  getRandomKey(){
+    return this.words[Math.floor(Math.random()*this.words.length)];
+  }
+
+  getRandomWord(key){
+    const words = this.chains[key];
+    if (!words[0] && words.length === 1) {
+      return undefined;
+    }
+    else {
+      const randomIdx = Math.floor(Math.random()*(words.length));
+      return this.chains[key][randomIdx];
+    }
+    //console.log(Math.floor(Math.random()*(words.length)))
+    //const randomIdx = Math.floor(Math.random()*(words.length));
+    //return this.chains[key][randomIdx];
+  }
+
+  /** return random text from chains */
   
 	makeText(numWords = 100) {
-	  // TODO
+    let currentKey = this.getRandomKey();
+    let sentence = currentKey;
+    for(let i=0; i<numWords; i++){
+      const currentWord = this.getRandomWord(currentKey);
+      if (!currentWord) break;
+      sentence += (' ' + currentWord);
+      currentKey = currentWord;
+    }
+    return sentence;
 	}
   }
   
